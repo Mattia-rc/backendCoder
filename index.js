ea336436ec591427e36649118f3d5c51a19c63d9
Mattia Bagni <matutebagni@gmail.com>
2023-04-08 19:42:31 -0300

    Primera entrega completa

--- index.js
+++ index.js
@@ -1,49 +1,59 @@
 
-class ProductManager{
-    constructor(){
+
+
+
+
+class ProductManager {
+    constructor() {
         this.productos = [];
     }
 
-        getEvents(){
-             console.log(this.productos) 
-            return this.productos
+    addProduct({ title, description, price, thumbnail, stock }) {
 
-        }
-
-         getProductsByid(id){
-            this.productos.find((p)=>p.id === id)
-
-            console.log(this.productos);
-            return this.productos
-         }
-
-    addProducts({title,description, price, thumbnail,stock}){  
-        let id 
-        if(this.productos.length===0){
+        let id
+        if (this.productos.length === 0) {
             id = 1;
-           
-        }else{
-            let lastProducto = this.productos[this.productos.length-1];
-            lastProducto.id+1;
-            id = lastProducto.id+1;
+
+        } else {
+            let lastProducto = this.productos[this.productos.length - 1];
+          
+            id = lastProducto.id + 1;
         }
-        
-     
 
+        let producto = { title, description, price, thumbnail, stock, id}
+        this.productos.push(producto);
 
-
-
-        let producto = {title,description, price, thumbnail,stock, id};
-        this.productos.push(producto); 
     }
+    getProducts() {
+/*         console.log(this.productos) */
+        return this.productos
+
+    }
+
+    getProductById(id) {
+
+    
+        let productoId = this.productos.find((p) => p.id === id)
+        console.log(productoId);
+        
+            if(!productoId){
+                console.error("NOT FOUND");
+                     
+            }
+
+            return this.productoId
+
+    }
+
 }
 
 
 let producto = new ProductManager;
+producto.addProduct({ title: "iphone", description: "iphone 13 mini", price: 1200, thumbnail: "img", stock: 2 });
+producto.addProduct({ title: "iphone", description: "iphone 14", price: 1500, thumbnail: "img", stock: 5 });
+producto.addProduct({ title: "iphone", description: "iphone 12 mini", price: 1000, thumbnail: "img", stock: 4 });
 
-producto.addProducts({title: "iphone", description: "iphone 13 mini", price: 1200, thumbnail:"img", stock: 2});
-producto.addProducts({title: "iphone", description: "iphone 14", price: 1500, thumbnail:"img", stock: 5});
-producto.addProducts({title: "iphone", description: "iphone 12 mini", price: 1000, thumbnail:"img", stock: 4});
 
-producto.getEvents(); 
-producto.getProductsByid(2)
+producto.getProducts()
+
+producto.getProductById(2)
